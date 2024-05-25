2e5949c97c68d1666f1e250d15520178a4a24887
Snarling <70073449+LivioLipani@users.noreply.github.com>
2024-05-25 20:27:07 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -73,7 +73,7 @@ vector = PineconeVectorStore(index_name=index_name, embedding=OpenAIEmbeddings()
 retriever = vector.as_retriever(search_type="mmr", search_kwargs={"k": 4})
 
 # Initialize the model
-model = ChatOpenAI(model="gpt-3.5-turbo", streaming=True)
+model = ChatOpenAI(model="gpt-4o", streaming=True)
 
 
 # Define math tool
